d6798b205f8d125741ba64479d8fda02fe39305b
DUYNGO14 <ngoduy07142003@gmail.com>
2025-01-02 09:56:14 +0700

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.scss";
-
+import ColorBox from "./components/ColorBox";
+import ToDoList from "./components/ToDoList";
 function App() {
-  return <div className="app">Wellcome to react hook</div>;
+  return <div className="app">
+    <ColorBox />
+    <ToDoList/>
+  </div>;
 }
 
 export default App;
